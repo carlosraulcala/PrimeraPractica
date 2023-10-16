e1d9e12a4c2842287102074a8d945d8b27f4acef
carlosraulcala <calacamatacarlosraul@gmail.com>
2023-10-16 10:41:13 -0400

    agregando materias

---
+++ ProyectoWebApp/Models/Materia.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ProyectoWebApp.Models
+{
+    public class Materia
+    {
+        [Key]
+        public int idMateia { get; set; }
+        public string Nombre { get; set; }
+        public int idDocente { get; set; }
+    }
+}
